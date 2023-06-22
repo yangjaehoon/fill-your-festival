bad15c6764132cafbf2d0882b6502427e39b9496
Jaehoon Yang <lljhms@naver.com>
2023-06-22 21:38:28 +0900

    login

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:feple/screen/home_screen.dart';
+import 'package:feple/screen/login.dart';
+
 import 'package:flutter/material.dart';
 
 void main(){
   runApp(
     MaterialApp(
-      home: HomeScreen(),
+      home: LogIn(), //로그인 화면 볼꺼면 주석 풀어
+      //home: HomeScreen(), //슬라이딩 하는 메인 화면 볼꺼면 이 주석
     ),
   );
 }
