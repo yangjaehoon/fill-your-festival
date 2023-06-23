ab22e3c7e2dbfa6beb8d4bcfb0c709125d6a50df
Jaehoon Yang <lljhms@naver.com>
2023-06-23 18:30:58 +0900

    login_button_refactoring

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:feple/screen/home_screen.dart';
 
+
 class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Dice game',
+      title: 'Login',
       home: LogIn(),
     );
   }
 }
 
 class LogIn extends StatefulWidget{
+
   @override
   State<LogIn> createState() => _LogInState();
 }
@@ -41,83 +45,89 @@ class _LogInState extends State<LogIn> {
       ),
       body: Builder(
         builder:(context) {
-          return SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                Padding(padding: EdgeInsets.only(top: 50)),
-                Center(
-                  child: Image(
-                    image: AssetImage('asset/img/logo_center.png'),
-                    width: 170.0,
-                    height: 190.0,
-                  ),
-                ),
-                Form(child: Theme(
-                    data: ThemeData(
-                        primaryColor: Colors.teal,
-                        inputDecorationTheme: InputDecorationTheme(
-                            labelStyle: TextStyle(
-                                color: Colors.teal,
-                                fontSize:15.0
-                            )
-                        )
+          return GestureDetector(
+            onTap: (){
+              FocusScope.of(context).unfocus();
+            },
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  Padding(padding: EdgeInsets.only(top: 50)),
+                  Center(
+                    child: Image(
+                      image: AssetImage('asset/img/logo_center.png'),
+                      width: 170.0,
+                      height: 190.0,
                     ),
-                    child: Container(
-                      padding: EdgeInsets.all(40.0),
-                      child: Column(
-                        children: <Widget>[
-                          TextField(
-                            controller: controller, //아이디 부분
-                            decoration: InputDecoration(
-                                labelText: 'Enter email(dice)'
-                            ),
-                            keyboardType: TextInputType.emailAddress,
-                          ),
-                          TextField(
-                            controller: controller2, //비밀번호 부분
-                            decoration: InputDecoration(
-                                labelText: 'Enter Password(1234)'
-                            ),
-                            keyboardType: TextInputType.text,
-                            obscureText: true, //비밀번호 안보이게 타이핑 하는거
-                          ),
-                          SizedBox(height: 40.00), //  비밀번호창과 버튼 사이 띄우즈기 위한거(padding안쓰기 위함)
-                          ButtonTheme(
-                              minWidth: 100.0,
-                              height: 50.0,
-                              child: ElevatedButton(
-                                style:ElevatedButton.styleFrom(
-                                  backgroundColor: Colors.blueAccent,
-                                ),
-                                child: Icon(Icons.arrow_forward,
-                                    color: Colors.white,
-                                    size:35.0
-                                ),
-                                onPressed: (){
-                                  if(controller.text == 'dice' && controller2.text == '1234') {
-                                    Navigator.push(context,
-                                        MaterialPageRoute(builder: (BuildContext context)=>HomeScreen()
-                                        )
-                                    );
-                                  }
-                                  else if(controller.text != 'dice' && controller2.text == '1234'){
-                                    showSnackBar1(context);
-                                  }else if(controller.text != 'dice' && controller2.text == '1234'){
-                                    showSnackBar2(context);
-                                  }else{
-                                    showSnackBar3(context);
-                                  }
-
-
-                                },
+                  ),
+                  Form(child: Theme(
+                      data: ThemeData(
+                          primaryColor: Colors.teal,
+                          inputDecorationTheme: InputDecorationTheme(
+                              labelStyle: TextStyle(
+                                  color: Colors.teal,
+                                  fontSize:15.0
                               )
                           )
-                        ],
                       ),
-                    )
-                )
-                ),
-              ],
+                      child: Container(
+                        padding: EdgeInsets.all(40.0),
+                        child: Column(
+                          children: <Widget>[
+                            TextField(
+                              autofocus: true,
+                              controller: controller, //아이디 부분
+                              decoration: InputDecoration(
+                                  labelText: 'Enter email(dice)'
+                              ),
+                              keyboardType: TextInputType.emailAddress,
+                            ),
+                            TextField(
+                              controller: controller2, //비밀번호 부분
+                              decoration: InputDecoration(
+                                  labelText: 'Enter Password(1234)'
+                              ),
+                              keyboardType: TextInputType.text,
+                              obscureText: true, //비밀번호 안보이게 타이핑 하는거
+                            ),
+                            SizedBox(height: 40.00), //  비밀번호창과 버튼 사이 띄우즈기 위한거(padding안쓰기 위함)
+                            ButtonTheme(
+                                minWidth: 100.0,
+                                height: 50.0,
+                                child: ElevatedButton(
+                                  style:ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.blueAccent,
+                                  ),
+                                  child: Icon(Icons.arrow_forward,
+                                      color: Colors.white,
+                                      size:35.0
+                                  ),
+                                  onPressed: (){
+                                    if(controller.text == 'dice' && controller2.text == '1234') {
+                                      Navigator.push(context,
+                                          MaterialPageRoute(builder: (BuildContext context)=>HomeScreen()
+                                          )
+                                      );
+                                    }
+                                    else if(controller.text != 'dice' && controller2.text == '1234'){
+                                      showSnackBar1(context);
+                                    }else if(controller.text != 'dice' && controller2.text == '1234'){
+                                      showSnackBar2(context);
+                                    }else{
+                                      showSnackBar3(context);
+                                    }
+
+
+                                  },
+                                )
+                            )
+                          ],
+                        ),
+                      )
+                  )
+                  ),
+                ],
+              ),
             ),
           );
         },
