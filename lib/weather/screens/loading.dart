a6d910def49e97076b9af00a5abd1b2131e05570
Jaehoon Yang <lljhms@naver.com>
2023-06-27 16:34:56 +0900

    weather

---
+++ lib/weather/screens/loading.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class Loading extends StatefulWidget {
+  const Loading({super.key});
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+  void getLocation() async{
+    LocationPermission permission = await Geolocator.requestPermission();
+    Position position = await Geolocator.
+    getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    print(position);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: (){
+            getLocation();
+          },
+          child: Text(
+            'Get my location',
+            style: TextStyle(
+              color: Colors.white,
+              backgroundColor: Colors.blue,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
